829775c12a7d34b4aa47ef81783d2f1436fa24b0
DiogoAlexandrinoDias <raibarreto2006@gmail.com>
2025-09-23 21:00:41 -0300

    Exercicios em sala de aula.

---
+++ exercicios-aluno/lacos-de-repeticao/lacos-de-repeticao  - For 3/main.js
@@ -0,0 +1,5 @@
+let numero = prompt('Digite um numero:');
+
+do { 
+   numero = prompt ('Digite um numero')
+} while (numero != '0' && numero != '1'); 
--- exercicios-aluno/lacos-de-repeticao/lacos-de-repeticao - 7/main.js
+++ exercicios-aluno/lacos-de-repeticao/lacos-de-repeticao - 7/main.js
@@ -1,6 +1,14 @@
-let numero = '()';
-let maiornumero;
+let numero = 0;
+let maiorNumero = 0;
+let contador = 0;
 
 do {
-   numero = prompt ('Digite um numero:')
-} while ( + maiornumero);
+   numero = prompt ('Insira um numero');
+
+      if (numero > maior numero) {
+         maiorNumero = numero;
+      }
+      contador = contador + 1;
+} while ( contador < 5);
+
+console.log ('O maior numero inserido foi' + maiorNumero); 
