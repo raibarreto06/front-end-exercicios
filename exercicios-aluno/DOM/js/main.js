01b33086590a9114ca84e99192ddf504f69b6304
DiogoAlexandrinoDias <raibarreto2006@gmail.com>
2025-09-25 21:55:49 -0300

    Iniciando exercicio Dom.

---
+++ exercicios-aluno/DOM/js/main.js
@@ -0,0 +1,10 @@
+let cortico = document.getElementById('o-cortico');
+    console.log(cortico);
+
+let tag = document.getElementsByTagName(`<a>`);
+    console.log(tag);
+
+let vidaseca = document.querySelector(`vidas-secas`);
+    console.log(vidaseca);
+
+let navList = document.querySelector(`.footer-links.nav-list`);
