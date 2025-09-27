4301583bc6788415f2d190c2dfbc0aaeaf9289be
DiogoAlexandrinoDias <raibarreto2006@gmail.com>
2025-09-26 21:43:57 -0300

    Finalizando exercicio DOM

--- exercicios-aluno/DOM/js/main.js
+++ exercicios-aluno/DOM/js/main.js
@@ -1,10 +1,34 @@
-let cortico = document.getElementById('o-cortico');
-    console.log(cortico);
+let livrol = document.getElementById('o-cortico');
+    console.log('typeof livrol: ', typeof livrol);
 
-let tag = document.getElementsByTagName(`<a>`);
-    console.log(tag);
+let links = document.getElementsByTagName('a');
+    console.log('links:', links);
 
-let vidaseca = document.querySelector(`vidas-secas`);
-    console.log(vidaseca);
+let livro2 = document.querySelector('#vidas-secas');
+    console.log('livro2:', livro2);
 
-let navList = document.querySelector(`.footer-links.nav-list`);
+let navlistFooter= document.querySelector('.footer-links .nav-list');
+    console.log('navlistFooter:', navlistFooter);
+
+let navlistHeader= document.querySelector('.header-links .nav-list');
+    console.log('navlistHeader:', navlistHeader);
+
+
+livrol.style.color = '#AA0000'
+
+let paragrafo = livrol.querySelector('p');
+paragrafo.style.color = 'red' ;
+
+livro2.className = 'card-livro fora-de-estoque';
+console.log('livro2.class.Name:', livro2.class.Name:);
+
+let capaDoLivro = document.querySelector('.capa-livro');
+capaDoLivro.setAttribute('src','images/capas/o-cortico.jpeg');
+
+let novoParagrafo = document.createAttribute ('p');
+
+novoParagrafo.textContent = 'Clarice Lispector';
+ 
+ley tituloLivro3 = document.querySelector('#dom-casmuro .titulo-livro');
+
+tituloLivro3.after(novoParagrafo);
